56775ed8dfbf10ceb606596c1cb2c0538b61a626
charunpara <charunupara1@gmail.com>
2019-04-21 12:59:27 -0500

    implement sorts

---
+++ src/edu/grinnell/sortingvisualizer/sorts/Driver.java
@@ -0,0 +1,22 @@
+package edu.grinnell.sortingvisualizer.sorts;
+
+public class Driver {
+  public static void main(String[] args) {
+    Integer[] arr = {3,9,2,5};
+    
+    printArray(arr);
+    
+    Sorts.quickSort(arr, 0, 4);
+    
+    
+    printArray(arr);
+  }
+  
+  public static void printArray(Integer arr[]) 
+  { 
+      int n = arr.length; 
+      for (int i=0; i<n; ++i) 
+          System.out.print(arr[i]+" "); 
+      System.out.println(); 
+  } 
+}
--- src/edu/grinnell/sortingvisualizer/sorts/Sorts.java
+++ src/edu/grinnell/sortingvisualizer/sorts/Sorts.java
@@ -1,22 +1,19 @@
 package edu.grinnell.sortingvisualizer.sorts;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sorts<T extends Comparable<T>> {
-  
-  public static List<Integer> list;
-  
+
   public static <T extends Comparable<T>> void selectionSort(T[] arr) {
     int length = arr.length;
 
     int minIndex;
-    //int maxIndex;
+
 
     for (int i = 0; i < length - 1; i++) {
       minIndex = i;
       for (int j = i + 1; j < length; j++) {
-        list.add(j);
-        list.add(minIndex);
         if (arr[j].compareTo(arr[minIndex]) < 0) {
           minIndex = j;
         } // if
@@ -28,49 +25,72 @@ public class Sorts<T extends Comparable<T>> {
 
     } // for
   } // selectionSort(T[] arr)
+
+  /*
+   * 
+   * 
+   * 
+   * 
+   */
   
   public static <T extends Comparable<T>> void insertionSort(T[] arr) {
     int length = arr.length;
-    
+
     for (int i = 0; i < length; i++) {
       T key = arr[i];
+      int j = i - 1;
       
-      int j = i-1;
-      
-      while(j >= 0 && (arr[j].compareTo(key)) > 0) {
-        arr[j+1] = arr[j];
+      while (j >= 0 && (arr[j].compareTo(key)) > 0) {
+        arr[j + 1] = arr[j];
         --j;
       } // while
-      arr[j+1] = key;
-    } // for 
+      arr[j + 1] = key;
+    } // for
   } // insertionSort(T[] arr)
+
+
+  /*
+   * 
+   * 
+   * 
+   * 
+   */
   
+  public static<T extends Comparable<T>> void mergeSort(T[] arr, int l, int r) {
+    
+  }
+  
+  public static <T extends Comparable<T>> void merge(T[] arr, int low, int middle, int high) {
+    int length = (high - low) + 1;
+    
+    ArrayList<T> temp = new ArrayList<T>(length);
+    
+    int i = low;
+    int j = middle + 1;
+    T lowValue = null;
+    T highValue = null;
+    
+    
+  }
   
   /*
-  public static <T extends Comparable<T>> void mergeSort(T[] arr, int size) {
-    if (size < 2) {
-      return;
-    }
-    
-    int mid = size / 2;
-    T[] left = new T[mid];
-    
-    T[] right = new T[size - mid];
-  } // mergeSort(T[] arr)
-  */
+   * 
+   * 
+   * 
+   * 
+   */
   
-  public static <T extends Comparable<T>> void quickSort (T[] arr, int low, int high) {
+  public static <T extends Comparable<T>> void quickSort(T[] arr, int low, int high) {
     if (low < high) {
       int index = partition(arr, low, high);
-      
-      quickSort(arr,low, index-1);
-      quickSort(arr, index+1, high);
+      quickSort(arr, low, index - 1);
+      quickSort(arr, index + 1, high);
     } // if
   } // quickSort(T[] arr, int low, int high)
-  
+
   public static <T extends Comparable<T>> int partition(T[] arr, int low, int high) {
     T pivot = arr[high];
-    int i = (low-1);
+    int i = (low - 1);
     for (int j = low; j < high; j++) {
       if ((arr[j].compareTo(pivot) == 0) || (arr[j].compareTo(pivot) > 0)) {
         i++;
@@ -80,24 +100,36 @@ public class Sorts<T extends Comparable<T>> {
         arr[j] = temp;
       } // if
     } // for
-    
+
     // swap pivot
-    T temp = arr[i+1];
-    arr[i+1] = arr[high];
+    T temp = arr[i + 1];
+    arr[i + 1] = arr[high];
     arr[high] = temp;
-    
-    return i+1;
+
+    return i + 1;
   } // partition(T[] arr, int low, int high)
   
+  
+  
+  /*
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   */
+
   public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
     for (int i = 0; i < arr.length - 1; i++) {
-      for(int j = 0; j < arr.length - i - 1; j++)
-        if(arr[j].compareTo(arr[j + 1]) > 0) {
+      for (int j = 0; j < arr.length - i - 1; j++)
+        if (arr[j].compareTo(arr[j + 1]) > 0) {
           T temp = arr[j];
           arr[j] = arr[j + 1];
-          arr[j+1] = temp;
+          arr[j + 1] = temp;
         }
     }
   }
-  
+
 } // Sorts
